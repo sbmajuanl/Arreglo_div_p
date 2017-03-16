a8f9aa9bc7b901bcccdf78470e15a7f1f009f48d
Brilly Majuan <sbmajuanl@gmail.com>
2017-03-16 18:32:20 -0500

    Subiendo archivo

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+var datos = [
+							{ nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "10 de julio" },
+							{ nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" },
+							{ nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" },
+							{ nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
+							{ nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" }
+						];
+
+datos.forEach(function(element,index,array){
+  var div=document.createElement("div");
+  div.classList.add("div-contenedor");
+
+  for (var atributo in element){
+  var p=document.createElement("p");
+  p.innerHTML=atributo + ":" + element[atributo];
+  div.appendChild(p);
+  }
+  contenedor.appendChild(div);
+});

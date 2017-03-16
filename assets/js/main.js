ca45d2bbefe5d755eece9c2fb43332f5352e74ca
Brilly Majuan <sbmajuanl@gmail.com>
2017-03-16 18:34:25 -0500

    Subiendo cambios en js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
-var datos = [
-							{ nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "10 de julio" },
+var datos = [{ nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "10 de julio" },
 							{ nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" },
 							{ nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" },
 							{ nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
